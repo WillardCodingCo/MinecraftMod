409c46ab8ad7c49e03524039fc418ea657cc3344
Sam Willard <sjwillar@mtu.edu>
2018-12-23 21:56:35 -0500

    video two update with ruby, ruby texture not working

--- src/main/java/com/willardcodingco/minecraftmod/proxy/CommonProxy.java
+++ src/main/java/com/willardcodingco/minecraftmod/proxy/CommonProxy.java
@@ -1,5 +1,10 @@
 package com.willardcodingco.minecraftmod.proxy;
 
-public class CommonProxy {
+import net.minecraft.item.Item;
 
+public class CommonProxy
+{
+	
+	public void registerItemRenderer(Item item, int meta, String id) { }
+	
 }
